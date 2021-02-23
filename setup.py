4b4228208d0bebd76319f68564b989fea86f2194
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-02-23 23:17:29 +0300

    Added slot type.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["NamedEntityRecognition/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-NamedEntityRecognition-Cy',
-    version='1.0.3',
+    version='1.0.4',
     packages=['NamedEntityRecognition'],
     package_data={'NamedEntityRecognition': ['*.pxd', '*.pyx', '*.c', '*.py']},
     url='https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Cy',
